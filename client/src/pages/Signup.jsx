e20f5a023854dac00d1d13766d8df9e7c24cc64c
MScott-Dev <MScott0199@gmail.com>
2024-04-19 22:20:39 -0400

    Error handling and info given to user on the type of error

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -6,27 +6,38 @@ import { ADD_USER } from '../utils/mutations';
 import Starfruit from "../../public/images/Starfruit.png"
 import bundles from '../utils/bundles';
 
-// TODO message for each error to let the user know what happened
 function Signup() {
   const [formState, setFormState] = useState({ email: '', password: '' });
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const [addUser] = useMutation(ADD_USER);
-  
+  const [addUser, { data, loading, error }] = useMutation(ADD_USER);
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    const mutationResponse = await addUser({
+
+    try {
+      const mutationResponse = await addUser({
       variables: {
         email: formState.email,
         password: formState.password,
         username: formState.username,
         bundles: bundles
       },
+      onError(err) {
+        if (err == `ApolloError: E11000 duplicate key error collection: Stardew-tracker.users index: email_1 dup key: { email: "${formState.email}" }`) {
+        setErrorMessage("Email already in use, please try a different one!");
+      } else if (err == `ApolloError: E11000 duplicate key error collection: Stardew-tracker.users index: username_1 dup key: { username: "${formState.username}" }`) {
+        setErrorMessage('Username is taken, please try a different one!');
+      } else {
+        setErrorMessage("An error happened");
+      }
+      },
     });
     const token = mutationResponse.data.addUser.token;
     Auth.login(token);
   
-    
-    window.location.assign(`./getstarted`)
+    window.location.assign(`./getstarted`);
+    } catch (error) {
+    }
   };
 
   const handleChange = (event) => {
@@ -39,13 +50,16 @@ function Signup() {
 
     return (
     <div className="flex flex-wrap justify-center content-center h-full w-screen">
-        <section className='border-8 border-yellow-200 bg-yellow-50 h-[31rem] lg:w-[28rem] md:w-[28rem] sm:w-[28rem] w-[21rem] p-5 rounded drop-shadow-lg dark:bg-darkGray'>
+        <section className='border-8 border-yellow-200 bg-yellow-50 h-[33rem] lg:w-[28rem] md:w-[28rem] sm:w-[28rem] w-[21rem] p-5 rounded drop-shadow-lg dark:bg-darkGray'>
         <section className='h-24 border-b-4 border-yellow-200 flex justify-center flex-wrap content-center'>
             <h2 className='text-center text-4xl sm:text-5xl md:text-5xl lg:text-5xl text-yellow-300 font-bold drop-shadow-lg'>Sign up!</h2>
             <img className="size-10 ml-3 animate-bounce" src={Starfruit} alt="Chicken"></img>
         </section>
         
         <form onSubmit={handleFormSubmit}>
+          <section>
+            <p id='error' className='text-red-600 italic underline'>{errorMessage}</p>
+          </section>
             <div className="flex flex-wrap justfity-evenly content-center gap-3 h-20 mt-3">
             <div className="mb-5  w-full">
                 <label className="block text-sm font-medium text-gray-600 dark:text-offWhite" htmlFor="email">Username</label>
